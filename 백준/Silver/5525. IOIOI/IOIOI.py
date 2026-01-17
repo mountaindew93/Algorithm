80fa2fa0d55f91170658d287cbf7381ff40cafec
Si Kyeong Yang <yangsikyeong@gmail.com>
2026-01-18 01:18:57 +0900

    [Silver I] Title: IOIOI, Time: 324 ms, Memory: 33388 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,10 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 m = int(input())
 s = input()
 
-result=0
-str = 'IOI'+'OI'*(n-1)
+result=0; chk=0; flag=0
+str = 'IOI'
 
-for i in range(m):
-    if s[i:i+len(str)]==str: result+=1
+while(chk!=m):
+    if s[chk:chk+3] == str:
+        chk+=2; flag +=1
+        if flag>=n: result+=1
+    else: chk+=1; flag=0
+
+
 print(result)
